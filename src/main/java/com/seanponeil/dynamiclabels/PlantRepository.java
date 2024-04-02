6d14de01e33b63671fac632c7ebbda0c6089424c
Sean O'Neil <oneilse14@gmail.com>
2024-04-01 21:24:30 -0400

    Remove unused method

--- src/main/java/com/seanponeil/dynamiclabels/PlantRepository.java
+++ src/main/java/com/seanponeil/dynamiclabels/PlantRepository.java
@@ -1,13 +1,6 @@
 package com.seanponeil.dynamiclabels;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.data.neo4j.repository.query.Query;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
 public interface PlantRepository extends Neo4jRepository<Plant, String> {
-    @Query("MATCH (p:Plant) RETURN p")
-    List<Plant> findAllPlants();
 }
